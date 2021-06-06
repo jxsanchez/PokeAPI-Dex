3c177642bc0764825a4daa9a887826c1450a404a
Jesus Sanchez <jxasanchez@gmail.com>
2021-06-06 08:43:51 -0700

    fix login https issue

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -11,11 +11,17 @@
     <div class="container teams-container d-flex flex-column">
         <h6>Check out other trainers' teams!</h6>
         @foreach($allTeams as $key => $team)
-            <div class="container team-info" style="padding: 10px 0;">
-                <p class="trainer-name">TRAINER NAME: {{$team->userName}}</p>
-                <div class="row team-container d-flex justify-content-start">
+        <!-- 0 padding on left/right to line up pokemon names on left -->
+            <div class="container team-info" style="padding: 10px 0; width: 100%;">
+                @if(Auth::check() && $team->userId != Auth::id())
+                    <p class="trainer-name">TRAINER NAME: {{$team->userName}}</p>
+                @else
+                    <p class="trainer-name">YOUR TEAM</p>
+                @endif
+
+                <div class="row team-container d-flex justify-content-start" style="width: fit-content">
                     @foreach($team->team as $pokemon)
-                        <div class="col-sm-2 pokemon-icon-container d-flex flex-column justify-content-center align-items-start">
+                        <div class="pokemon-icon-container team-pokemon-container col-sm-2 d-flex flex-column justify-content-center align-items-start">
                             <a href="/pokemon/{{$pokemon->name}}" class="d-flex flex-column team-pokemon-link">
                                 <img class="pokemon-icon" src="{{$pokemon->sprite_url}}" alt="">
                                 <p class="pokemon-name">{{$pokemon->name}}</p>
@@ -24,7 +30,7 @@
                     @endforeach
                 </div>
 
-                <div class="teams-stats-container d-flex justify-content-start">
+                <div class="team-stats-container d-flex justify-content-start">
                     <p>
                         {{$team->likeCount}} Likes
                         
@@ -33,7 +39,7 @@
                         @endif
                     </p>
                     <p class="dot-divider"> • </p>
-                    <p>{{$team->updated_at}}</p>
+                    <p class="d-flex align-items-center">{{$team->updated_at}}</p>
                 </div>
             </div>
 
