cb05650859257b18bad90983749a879a57100749
Jesus Sanchez <72468046+jxsanchez@users.noreply.github.com>
2021-06-30 07:02:35 -0700

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="footer d-flex flex-column align-items-center justify-content-center">
     <p>
-        Made by <a href="https://jxsdev.com/" target="blank">Jesus Sanchez</a>
+        Made by <a href="https://mysterious-lake-68452.herokuapp.com/" target="blank">Jesus Sanchez</a>
         using <a href="https://pokeapi.co/" target="blank">PokéAPI</a>
     </p>
 </footer>
